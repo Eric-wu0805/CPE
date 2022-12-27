6f6fb64d63bdbc9895a367a092a5a2d1994a5ee6
Eric-wu0805 <96850622+Eric-wu0805@users.noreply.github.com>
2022-12-27 09:14:16 +0800

    Add files via upload

---
+++ 12019 - Doom's Day Algorithm.cpp
@@ -0,0 +1,245 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int case1;
+	cin>>case1;
+	while(case1--){
+		int a,b;
+		while(cin>>a>>b){
+			string cal[7]={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+			if(a==1){
+				if(b%7==3){
+					cout<<cal[0]<<endl;
+				}else if(b%7==4){
+					cout<<cal[1]<<endl;
+				}else if(b%7==5){
+					cout<<cal[2]<<endl;
+				}else if(b%7==6){
+					cout<<cal[3]<<endl;
+				}else if(b%7==0){
+					cout<<cal[4]<<endl;
+				}else if(b%7==1){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+			
+			if(a==2){
+				if(b%7==0){
+					cout<<cal[0]<<endl;
+				}else if(b%7==1){
+					cout<<cal[1]<<endl;
+				}else if(b%7==2){
+					cout<<cal[2]<<endl;
+				}else if(b%7==3){
+					cout<<cal[3]<<endl;
+				}else if(b%7==4){
+					cout<<cal[4]<<endl;
+				}else if(b%7==5){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+			
+			if(a==3){
+				if(b%7==0){
+					cout<<cal[0]<<endl;
+				}else if(b%7==1){
+					cout<<cal[1]<<endl;
+				}else if(b%7==2){
+					cout<<cal[2]<<endl;
+				}else if(b%7==3){
+					cout<<cal[3]<<endl;
+				}else if(b%7==4){
+					cout<<cal[4]<<endl;
+				}else if(b%7==5){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+			
+			if(a==4){
+				if(b%7==4){
+					cout<<cal[0]<<endl;
+				}else if(b%7==5){
+					cout<<cal[1]<<endl;
+				}else if(b%7==6){
+					cout<<cal[2]<<endl;
+				}else if(b%7==0){
+					cout<<cal[3]<<endl;
+				}else if(b%7==1){
+					cout<<cal[4]<<endl;
+				}else if(b%7==2){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+			
+			
+			if(a==5){
+				if(b%7==2){
+					cout<<cal[0]<<endl;
+				}else if(b%7==3){
+					cout<<cal[1]<<endl;
+				}else if(b%7==4){
+					cout<<cal[2]<<endl;
+				}else if(b%7==5){
+					cout<<cal[3]<<endl;
+				}else if(b%7==6){
+					cout<<cal[4]<<endl;
+				}else if(b%7==0){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+				
+			if(a==6){
+				if(b%7==6){
+					cout<<cal[0]<<endl;
+				}else if(b%7==0){
+					cout<<cal[1]<<endl;
+				}else if(b%7==1){
+					cout<<cal[2]<<endl;
+				}else if(b%7==2){
+					cout<<cal[3]<<endl;
+				}else if(b%7==3){
+					cout<<cal[4]<<endl;
+				}else if(b%7==4){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+			
+						
+			if(a==7){
+				if(b%7==4){
+					cout<<cal[0]<<endl;
+				}else if(b%7==5){
+					cout<<cal[1]<<endl;
+				}else if(b%7==6){
+					cout<<cal[2]<<endl;
+				}else if(b%7==0){
+					cout<<cal[3]<<endl;
+				}else if(b%7==1){
+					cout<<cal[4]<<endl;
+				}else if(b%7==2){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+								
+			if(a==8){
+				if(b%7==1){
+					cout<<cal[0]<<endl;
+				}else if(b%7==2){
+					cout<<cal[1]<<endl;
+				}else if(b%7==3){
+					cout<<cal[2]<<endl;
+				}else if(b%7==4){
+					cout<<cal[3]<<endl;
+				}else if(b%7==5){
+					cout<<cal[4]<<endl;
+				}else if(b%7==6){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+									
+			if(a==9){
+				if(b%7==5){
+					cout<<cal[0]<<endl;
+				}else if(b%7==6){
+					cout<<cal[1]<<endl;
+				}else if(b%7==0){
+					cout<<cal[2]<<endl;
+				}else if(b%7==1){
+					cout<<cal[3]<<endl;
+				}else if(b%7==2){
+					cout<<cal[4]<<endl;
+				}else if(b%7==3){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+									
+			if(a==10){
+				if(b%7==3){
+					cout<<cal[0]<<endl;
+				}else if(b%7==4){
+					cout<<cal[1]<<endl;
+				}else if(b%7==6){
+					cout<<cal[2]<<endl;
+				}else if(b%7==0){
+					cout<<cal[3]<<endl;
+				}else if(b%7==1){
+					cout<<cal[4]<<endl;
+				}else if(b%7==2){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+									
+			if(a==11){
+				if(b%7==0){
+					cout<<cal[0]<<endl;
+				}else if(b%7==1){
+					cout<<cal[1]<<endl;
+				}else if(b%7==2){
+					cout<<cal[2]<<endl;
+				}else if(b%7==3){
+					cout<<cal[3]<<endl;
+				}else if(b%7==4){
+					cout<<cal[4]<<endl;
+				}else if(b%7==5){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+			
+			
+									
+			if(a==12){
+				if(b%7==5){
+					cout<<cal[0]<<endl;
+				}else if(b%7==6){
+					cout<<cal[1]<<endl;
+				}else if(b%7==0){
+					cout<<cal[2]<<endl;
+				}else if(b%7==1){
+					cout<<cal[3]<<endl;
+				}else if(b%7==2){
+					cout<<cal[4]<<endl;
+				}else if(b%7==3){
+					cout<<cal[5]<<endl;
+				}else{
+					cout<<cal[6]<<endl;
+				}
+			}
+		}
+		
+		
+	
+	}
+	return 0;
+} 
